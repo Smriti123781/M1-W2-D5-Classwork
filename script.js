c114fdfb90e3fec764a8d74ba0985ff9be9597a1
sansmr007 <sandeshthapa37@gmail.com>
2022-06-14 19:35:23 +0545

    This is the commit for week 2 day 5 classwork.

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 
-function experience() {
-    var drp = document.querySelector('.about-drop-content');
-    drp.classList.toggle('show');
+function experience(){
+    var drp = document.querySelector('.abt-drop-content');
+    drp.classList.toggle ('show');
+}
+
+function everything(){
+    var content = document.querySelector('.for-everything').classList;
+    alert(content);
+}
+
+function art(){
+    
+}
+
+function creative(){
+    
+}
+
+function design(){
+    
 }
